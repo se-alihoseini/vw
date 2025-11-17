f4cc9cec8c3a8e0e70a5b5a068bccab98ed4ab53
ali hosseini <se.alihoseini.work@gmail.com>
2025-11-17 16:43:16 +0330

    fix parse_objects func

--- scraper/management/commands/scrape_quotes.py
+++ scraper/management/commands/scrape_quotes.py
@@ -2,8 +2,8 @@ import asyncio
 import requests
 from scraper.schema import PageContext
 from typing import Iterable, List
-from urllib.parse import urlparse, urlunparse
 from django.core.management import BaseCommand, CommandError
+from bs4 import BeautifulSoup
 
 from scraper.classes import Normalizer, Hashing
 
@@ -18,19 +18,21 @@ class Command(BaseCommand):
         self.base_url = "https://quotes.toscrape.com/page/"
         self.start_page = 1
         self.session = requests.Session()
-
+        self.session.cookies.set(
+            "session",
+            "eyJjc3JmX3Rva2VuIjoidVFqZUl6QWNSdE1nWURORlBXQ3ZhU3BxRUhLc0JPR21VZlRueXdiaUxYa2hWZEpseHJvWiIsInVzZXJuYW1lIjoic2UuYWxpaG9zZWluaSJ9.aRr9ew.fcMQL-3DH_gG2NORMB5knaSM17I"
+        )
 
     def handle(self, *args, **options):
-        page_param = options["page_param"]
 
         try:
-            self.crawl(base_url=self.base_url, page_param=page_param, start_page=self.start_page)
+            self.crawl(base_url=self.base_url, start_page=self.start_page)
         except Exception as exc:
             raise CommandError(str(exc)) from exc
 
 
 
-    def crawl(self, base_url: str, page_param: str, start_page: int = 1):
+    def crawl(self, base_url: str, start_page: int = 1):
         handler = self.build_chain()
         page = start_page
 
@@ -44,8 +46,8 @@ class Command(BaseCommand):
             if html is None:
                 raise CommandError()
 
-            objects = self.parse_objects(html, context)
-            self.process_objects(objects, handler)
+            objects = self.parse_objects(html)
+            # self.process_objects(objects, handler)
             page += 1
 
 
@@ -68,9 +70,10 @@ class Command(BaseCommand):
 
 
 
-    def parse_objects(self, html: str, context: PageContext) -> List[dict]:
-        self.stdout.write(self.style.WARNING(f"No parser implemented yet for page {context.number}."))
-        return []
+    def parse_objects(self, html) -> List:
+        soup = BeautifulSoup(html, 'html.parser')
+        quotes = soup.find_all('div', class_='quote')
+        return quotes
 
 
 
